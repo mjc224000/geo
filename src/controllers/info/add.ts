a36da23250ded4ccbd7f7304c1e0751765ee5cc2
cc <397938635@qq.com>
2019-04-16 16:49:05 +0800

    init commite

--- src/controllers/info/add.ts
+++ src/controllers/info/add.ts
@@ -1,58 +1,70 @@
-import { Ctx, Param, Get, JsonController } from 'routing-controllers'
-import { UserData } from 'user-info'
-import { Context } from 'koa'
+import {Ctx, Param, Get, JsonController, QueryParam, Authorized} from 'routing-controllers'
+import {UserData} from 'user-info'
+import {Context} from 'koa'
 import UserInfo from '../../models/user/user-info'
 
 @JsonController('/add')
 export default class {
-  @Get('/:name/:age/:gender')
-  async router(
-    @Ctx() ctx: Context,
-    @Param('name') name: string,
-    @Param('age') age: number,
-    @Param('gender') gender: number
-  ) {
-    try {
-      if (!name || !gender) {
-        return {
-          code: 401,
-          message: '缺少参数',
+    @Get('/:name/:age/:gender')
+    async router(
+        @Ctx() ctx: Context,
+        @Param('name') name: string,
+        @Param('age') age: number,
+        @Param('gender') gender: number
+    ) {
+        console.log(name, age, gender)
+        try {
+            if (!name || !gender) {
+                return {
+                    code: 401,
+                    message: '缺少参数',
+                }
+            }
+
+            const entity: UserData = {
+                name,
+                gender,
+                age,
+            }
+
+            // 创建举报记录
+            createUser(entity).catch(err => {
+                console.error(ctx, err, {
+                    type: 'insert',
+                    ...entity,
+                })
+            })
+
+            return {
+                code: 200,
+            }
+        } catch (e) {
+
+            return {
+                code: 500,
+                message: '服务器错误',
+            }
         }
-      }
-
-      const entity: UserData = {
-        name,
-        gender,
-        age,
-      }
-
-      // 创建举报记录
-      createUser(entity).catch(err => {
-        console.error(ctx, err, {
-          type: 'insert',
-          ...entity,
-        })
-      })
-
-      return {
-        code: 200,
-      }
-    } catch (e) {
-      console.error(ctx, e)
-      return {
-        code: 500,
-        message: '服务器错误',
-      }
     }
-  }
+    @Authorized("guest")
+    @Get('/')
+    async geo(@Ctx() ctx: Context,
+             @QueryParam("name") name: string,
+             @QueryParam("age") age: number) {
+
+        console.log(name);
+        console.log(age);
+        return { message: 'ojbk: '+name}
+    }
+
 }
 
 /**
  * 创建一个新的用户
  */
-async function createUser({ name, gender }: UserData) {
-  return (await UserInfo.create({
-    name,
-    gender,
-  })).save()
+async function createUser({name, gender}: UserData) {
+    return (await UserInfo.create({
+        name,
+        gender,
+    })).save()
 }
